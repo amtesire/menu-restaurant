078dcdb6422e1a0ee872a61c27c5ce45954cbabc
amtesire <tesiregisele@gmail.com>
2020-10-13 21:28:53 +0200

    added some elements

---
+++ script.js
@@ -0,0 +1,120 @@
+$(document).ready(function() {
+    $(".wo, .wor").click(function() {
+      $("#wo").toggle();
+      $(".wor").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".wo1, .wor1").click(function() {
+      $("#wo1").toggle();
+      $(".wor1").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".wo2, .wor2").click(function() {
+      $("#wo2").toggle();
+      $(".wor2").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".wo3, .wor3").click(function() {
+      $("#wo3").toggle();
+      $(".wor3").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".wo4, .wor4").click(function() {
+      $("#wo4").toggle();
+      $(".wor4").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".wo5, .wor5").click(function() {
+      $("#wo5").toggle();
+      $(".wor5").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".wo6, .wor6").click(function() {
+      $("#wo6").toggle();
+      $(".wor6").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".wo7, .wor7").click(function() {
+      $("#wo7").toggle();
+      $(".wor7").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".wo8, .wor8").click(function() {
+      $("#wo8").toggle();
+      $(".wor8").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".wo9, .wor9").click(function() {
+      $("#wo9").toggle();
+      $(".wor9").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".wo10, .wor10").click(function() {
+      $("#wo10").toggle();
+      $(".wor10").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".wo11, .wor11").click(function() {
+      $("#wo11").toggle();
+      $(".wor11").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".wo12, .wor12").click(function() {
+      $("#wo12").toggle();
+      $(".wor12").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".wo13, .wor13").click(function() {
+      $("#wo13").toggle();
+      $(".wor13").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".wo14, .wor14").click(function() {
+      $("#wo14").toggle();
+      $(".wor14").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".wo15, .wor15").click(function() {
+      $("#wo15").toggle();
+      $(".wor15").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".si, .siz").click(function() {
+      $("#si").toggle();
+      $(".siz").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".si1, .siz1").click(function() {
+      $("#si1").toggle();
+      $(".siz1").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".si2, .siz2").click(function() {
+      $("#si2").toggle();
+      $(".siz2").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".si3, .siz3").click(function() {
+      $("#si3").toggle();
+      $(".siz3").toggle();
+    });
+  });
